26692b73855c10fbaefd719d5c64f38b3cd58a87
Aurich17 <chumpitazismael7@gmail.com>
2025-12-28 18:00:50 -0500

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+
 export default defineConfig({
   site: "https://whatsappautomation.online",
   integrations: [sitemap()],
